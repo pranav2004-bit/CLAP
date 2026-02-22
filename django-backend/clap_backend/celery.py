363980e9b8f97800c1c3f00d1119e52fd8580493
pranav2004-bit <pranavnath.k2004@gmail.com>
2026-02-22 13:02:44 +0530

    Add submission API endpoints and wire student submit action

---
+++ django-backend/clap_backend/celery.py
@@ -0,0 +1,16 @@
+"""Celery application for CLAP backend."""
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clap_backend.settings')
+
+app = Celery('clap_backend')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
